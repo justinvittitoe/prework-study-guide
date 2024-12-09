fbe3b9df878aea3a45fcce2bbda13e5dafe5400d
Jvitt <justinvittitoe7@gmail.com>
2024-12-09 01:05:30 -0700

    Add a function to randomly select a topic from our prework to study

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,25 @@
 const topics =["HTML", "CSS", "Git", "JavaScript"];
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+    for(var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-/*
-if(topics === 'HTML') {
-    console.log("Let's study HTML!");
-} else if(topics === 'CSS') {
-    console.log("Let's study CSS!");
-} else if(topics === 'Git') {
-    console.loy("Let's study Git!");
-} else if(topics === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else {
-    console.log('Please try again!');
+
+function selectTopic() {
+    if(randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if(randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if(randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if(randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
 }
-*/
+console.log('Here are the topics we learned through prework');
+listTopics()
+console.log('Which topic should we study first?');
+selectTopic()
